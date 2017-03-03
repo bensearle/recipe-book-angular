eb1367e7cc736f6cba00a292cbcca55ae829e940
Ben Searle <bensearle93@hotmail.co.uk>
2017-03-03 16:11:09 +0800

    Add recipe components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe';
+//import { RecipeItemComponent } from './recipe-item.component';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styles: []
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe = new Recipe ('Dummy', 'Dummy', 'http://www.pngall.com/wp-content/uploads/2016/03/Food-PNG.png');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe); 
+    console.log('**********', recipe);
+  }
+
+}
